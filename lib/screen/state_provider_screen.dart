4f102d7755af22b6d34763483347de06ac76ad6c
yeongwoo <jyy0223@naver.com>
2023-11-29 01:17:52 +0900

    [add] riverpod 를 이용한 상태관리 셋팅 구현

--- lib/screen/state_provider_screen.dart
+++ lib/screen/state_provider_screen.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_test/layout/default_layout.dart';
+import 'package:riverpod_test/riverpod/state_provider_screen.dart';
 
-class StateProviderScreen extends StatelessWidget {
+class StateProviderScreen extends ConsumerWidget {
   const StateProviderScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final provider = ref.watch(numberProvider);
+
     return DefaultLayout(
       title: 'StateProviderScreen',
-      body: Column(
-        children: [],
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              provider.toString(),
+              style: TextStyle(fontSize: 24.0),
+            ),
+          ],
+        ),
       ),
     );
   }
