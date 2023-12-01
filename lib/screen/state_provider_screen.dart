b9b9b020ff9f8c72fb246eb14926c28eed347a59
yeongwoo <jyy0223@naver.com>
2023-12-01 14:46:37 +0900

    [add] StateProvider UI 변경을 위한 watch와 특정 행위에 대한 기능 실행 read, read의 업데이트 방법 2가지 구현

--- lib/screen/state_provider_screen.dart
+++ lib/screen/state_provider_screen.dart
@@ -21,6 +21,62 @@ class StateProviderScreen extends ConsumerWidget {
               provider.toString(),
               style: TextStyle(fontSize: 24.0),
             ),
+            ElevatedButton(
+              onPressed: () {
+                ref.read(numberProvider.notifier).update((state) => state + 1);
+              },
+              child: Text('UP'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => _NextScreen(),
+                  ),
+                );
+              },
+              child: Text('NextScreen'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _NextScreen extends ConsumerWidget {
+  const _NextScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final provider = ref.watch(numberProvider);
+
+    return DefaultLayout(
+      title: 'StateProviderScreen',
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              provider.toString(),
+              style: TextStyle(fontSize: 24.0),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                ref.read(numberProvider.notifier).update((state) => state + 1);
+              },
+              child: Text('UP'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // state: 저장된 값
+                // 이것을 직접적으로 변경할 수 있다.
+                ref.read(numberProvider.notifier).state =
+                    ref.read(numberProvider.notifier).state - 1;
+              },
+              child: Text('DOWN'),
+            ),
           ],
         ),
       ),
