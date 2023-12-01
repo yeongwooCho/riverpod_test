ae6f3ba489198f4271827dc25597723de2c35376
yeongwoo <jyy0223@naver.com>
2023-12-01 20:45:29 +0900

    [add] Error에 대한 state.when 코드 결과 기록

--- lib/riverpod/future_provider.dart
+++ lib/riverpod/future_provider.dart
@@ -3,5 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 final multiplesFutureProvider = FutureProvider<List<int>>((ref) async {
   await Future.delayed(Duration(seconds: 2));
 
+  throw Exception('에러입니다.');
+
   return [1, 2, 3, 4, 5];
 });
