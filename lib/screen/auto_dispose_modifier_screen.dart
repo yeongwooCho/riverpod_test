e309a4f59c2d2d33114cb19d0e7888b7a2441120
yeongwoo <jyy0223@naver.com>
2023-12-02 00:08:04 +0900

    [add] autoDisposeModifierProvider 화면에 구현

--- lib/screen/auto_dispose_modifier_screen.dart
+++ lib/screen/auto_dispose_modifier_screen.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_test/layout/default_layout.dart';
+import 'package:riverpod_test/riverpod/auto_dispose_modifier_provider.dart';
 
-class AutoDisposeModifierScreen extends StatelessWidget {
+class AutoDisposeModifierScreen extends ConsumerWidget {
   const AutoDisposeModifierScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(autoDisposeModifierProvider);
+
     return DefaultLayout(
       title: 'AutoDisposeModifierScreen',
       body: Center(
-        child: Text(''),
+        child: state.when(
+          data: (data) {
+            return Text(
+              data.toString(),
+              textAlign: TextAlign.center,
+            );
+          },
+          error: (err, stack) => Text(err.toString()),
+          loading: () {
+            return CircularProgressIndicator();
+          },
+        ),
       ),
     );
   }
