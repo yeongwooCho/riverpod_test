c6c4e60bf892ff5f5cc7523606428a42d378cf29
yeongwoo <jyy0223@naver.com>
2023-12-02 01:42:15 +0900

    [add] SelectProviderScreen 선언 후 HomeScreen과 연결

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:riverpod_test/screen/auto_dispose_modifier_screen.dart';
 import 'package:riverpod_test/screen/family_modifier_screen.dart';
 import 'package:riverpod_test/screen/future_provider_screen.dart';
 import 'package:riverpod_test/screen/listen_provider_screen.dart';
+import 'package:riverpod_test/screen/select_provider_screen.dart';
 import 'package:riverpod_test/screen/state_notifier_provider_screen.dart';
 import 'package:riverpod_test/screen/state_provider_screen.dart';
 import 'package:riverpod_test/screen/stream_provider_screen.dart';
@@ -86,6 +87,16 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: Text('ListenProviderScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => SelectProviderScreen(),
+                ),
+              );
+            },
+            child: Text('SelectProviderScreen'),
           ),
         ],
       ),
---
+++ lib/screen/select_provider_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:riverpod_test/layout/default_layout.dart';
+
+class SelectProviderScreen extends StatelessWidget {
+  const SelectProviderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      title: 'SelectProviderScreen',
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
