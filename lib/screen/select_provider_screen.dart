9af60c5db908038246e6e980e345b6c46d87445c
yeongwoo <jyy0223@naver.com>
2023-12-02 02:21:55 +0900

    [add] select 기능으로 하나의 property만 watch하는 기능 구현

--- lib/screen/select_provider_screen.dart
+++ lib/screen/select_provider_screen.dart
@@ -1,15 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_test/layout/default_layout.dart';
+import 'package:riverpod_test/riverpod/select_provider.dart';
 
-class SelectProviderScreen extends StatelessWidget {
+class SelectProviderScreen extends ConsumerWidget {
   const SelectProviderScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    print('build');
+    final state = ref.watch(selectProvider.select((value) => value.isSpicy));
+
     return DefaultLayout(
       title: 'SelectProviderScreen',
-      body: Column(
-        children: [],
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(state.toString()),
+            // Text(state.name),
+            // Text(state.isSpicy.toString()),
+            // Text(state.hasBought.toString()),
+            ElevatedButton(
+              onPressed: () {
+                ref.read(selectProvider.notifier).toggleIsSpicy();
+              },
+              child: Text('Spicy Toggle'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                ref.read(selectProvider.notifier).toggleHasBought();
+              },
+              child: Text('HasBought Toggle'),
+            ),
+          ],
+        ),
       ),
     );
   }
