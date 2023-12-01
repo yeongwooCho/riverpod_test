b8b8d28936da7e482bdddf3af857b3bbdf7f9bfa
yeongwoo <jyy0223@naver.com>
2023-12-01 14:52:12 +0900

    [add] rename file of state_provider_screen -> state_provider

--- lib/screen/state_provider_screen.dart
+++ lib/screen/state_provider_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_test/layout/default_layout.dart';
-import 'package:riverpod_test/riverpod/state_provider_screen.dart';
+import 'package:riverpod_test/riverpod/state_provider.dart';
 
 class StateProviderScreen extends ConsumerWidget {
   const StateProviderScreen({super.key});
