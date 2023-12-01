91a0f5fa87a764f856a060235d348e04b979f513
yeongwoo <jyy0223@naver.com>
2023-12-01 20:21:13 +0900

    [add] future_provider_screen file

---
+++ lib/screen/future_provider_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:riverpod_test/layout/default_layout.dart';
+
+class FutureProviderScreen extends StatelessWidget {
+  const FutureProviderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      title: 'FutureProviderScreen',
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
