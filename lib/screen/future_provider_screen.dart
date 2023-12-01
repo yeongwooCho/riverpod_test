691164440c55d8e19c489749b3a741bd9396e6d7
yeongwoo <jyy0223@naver.com>
2023-12-01 20:39:15 +0900

    [add] state.when 을 통한 async value에 대한 화면 처리

--- lib/screen/future_provider_screen.dart
+++ lib/screen/future_provider_screen.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_test/layout/default_layout.dart';
+import 'package:riverpod_test/riverpod/future_provider.dart';
 
-class FutureProviderScreen extends StatelessWidget {
+class FutureProviderScreen extends ConsumerWidget {
   const FutureProviderScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(multiplesFutureProvider);
+
     return DefaultLayout(
       title: 'FutureProviderScreen',
       body: Column(
-        children: [],
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          state.when(
+            // data 는 당연히 반환 받은 state 값이다.
+            data: (data) {
+              return Text(
+                data.toString(),
+                textAlign: TextAlign.center,
+              );
+            },
+            error: (err, stack) => Text(err.toString()),
+            loading: () => const Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+        ],
       ),
     );
   }
