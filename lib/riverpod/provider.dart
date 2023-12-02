57fbfaf798476593c80a3e27dc742edc5a3e28aa
yeongwoo <jyy0223@naver.com>
2023-12-02 15:34:33 +0900

    [add] Provider 에서 StateProvider와 StateNotifierProvider 의 상태를 모두 watch 하여 상태 값들의 종속에 대한 제어를 구현

--- lib/riverpod/provider.dart
+++ lib/riverpod/provider.dart
@@ -1,8 +1,22 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_test/model/shopping_item_model.dart';
 import 'package:riverpod_test/riverpod/state_notifier_provider.dart';
 
-final filteredShoppingListProvider = Provider(
-  (ref) => ref.watch(shoppingListProvider),
+final filteredShoppingListProvider = Provider<List<ShoppingItemModel>>(
+  (ref) {
+    final filterState = ref.watch(filterProvider);
+    final shoppingListState = ref.watch(shoppingListProvider);
+
+    if (filterState == FilterState.all) {
+      return shoppingListState;
+    }
+
+    return shoppingListState
+        .where((element) => filterState == FilterState.spicy
+            ? element.isSpicy
+            : !element.isSpicy)
+        .toList();
+  },
 );
 
 enum FilterState {
