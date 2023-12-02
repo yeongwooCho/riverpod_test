821c9f2a1742e2e45998730338d323e58cf426b0
yeongwoo <jyy0223@naver.com>
2023-12-02 15:51:38 +0900

    [add] didUpdateProvider in ProviderObserver

--- lib/riverpod/provider_observer.dart
+++ lib/riverpod/provider_observer.dart
@@ -1,3 +1,10 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class Logger extends ProviderObserver {}
+class Logger extends ProviderObserver {
+  @override
+  void didUpdateProvider(ProviderBase provider, Object? previousValue,
+      Object? newValue, ProviderContainer container) {
+    print(
+        '[Provider Updated] provider: $provider / pv: $previousValue / nv: $newValue');
+  }
+}
