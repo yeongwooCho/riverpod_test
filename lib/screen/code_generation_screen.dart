9681f2b249009762163c2bbb770066e613757687
yeongwoo <jyy0223@naver.com>
2023-12-02 16:41:33 +0900

    [add] CodeGeneration 사용을 위한 화면 추가 및 HomeScreen 과 연결

---
+++ lib/screen/code_generation_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:riverpod_test/layout/default_layout.dart';
+
+class CodeGenerationScreen extends StatelessWidget {
+  const CodeGenerationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      title: 'CodeGenerationScreen',
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:riverpod_test/layout/default_layout.dart';
 import 'package:riverpod_test/screen/auto_dispose_modifier_screen.dart';
+import 'package:riverpod_test/screen/code_generation_screen.dart';
 import 'package:riverpod_test/screen/family_modifier_screen.dart';
 import 'package:riverpod_test/screen/future_provider_screen.dart';
 import 'package:riverpod_test/screen/listen_provider_screen.dart';
@@ -108,6 +109,16 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: Text('ProviderScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => CodeGenerationScreen(),
+                ),
+              );
+            },
+            child: Text('CodeGenerationScreen'),
           ),
         ],
       ),
