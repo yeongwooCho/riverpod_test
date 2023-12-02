708ecb99467f72d9f8e85174a6d65c99970a937a
yeongwoo <jyy0223@naver.com>
2023-12-02 15:21:58 +0900

    [add] 체크박스 리스트에서 PopUpMenuButton에 따른 리스트 화면 분기 셋팅 구현

--- lib/screen/provider_screen.dart
+++ lib/screen/provider_screen.dart
@@ -3,18 +3,46 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_test/layout/default_layout.dart';
 import 'package:riverpod_test/riverpod/provider.dart';
 
+import '../riverpod/state_notifier_provider.dart';
+
 class ProviderScreen extends ConsumerWidget {
   const ProviderScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final state = ref.watch(filteredShoppingListProvider);
-    print(state);
 
     return DefaultLayout(
+      actions: [
+        // FilterState 타입을 변경을 할 것이다.
+        PopupMenuButton<FilterState>(
+          // 넣고 싶은 option 들을 넣어주면 된다.
+          itemBuilder: (_) => FilterState.values
+              .map(
+                (e) => PopupMenuItem(
+                  value: e,
+                  child: Text(e.name),
+                ),
+              )
+              .toList(),
+          onSelected: (value) {
+            ref.read(filterProvider.notifier).update((state) => value);
+          },
+        )
+      ],
       title: 'ProviderScreen',
       body: ListView(
-        children: [],
+        children: state
+            .map((e) => CheckboxListTile(
+                  title: Text(e.name),
+                  value: e.hasBought,
+                  onChanged: (value) {
+                    ref
+                        .read(shoppingListProvider.notifier)
+                        .toggleHasBought(name: e.name);
+                  },
+                ))
+            .toList(),
       ),
     );
   }
