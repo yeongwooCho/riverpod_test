62047a7cfea60441e48ea4f45e1622519e1c2a23
yeongwoo <jyy0223@naver.com>
2023-12-02 17:19:43 +0900

    [add] code generation에 의해 생성된 gStateProvider를 Screen에 적용

--- lib/screen/code_generation_screen.dart
+++ lib/screen/code_generation_screen.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_test/layout/default_layout.dart';
+import 'package:riverpod_test/riverpod/code_generation_provider.dart';
 
-class CodeGenerationScreen extends StatelessWidget {
+class CodeGenerationScreen extends ConsumerWidget {
   const CodeGenerationScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    const textStyle = TextStyle(fontSize: 24.0);
+
+    final state1 = ref.watch(gStateProvider);
+
     return DefaultLayout(
       title: 'CodeGenerationScreen',
       body: Column(
-        children: [],
+        children: [
+          Text(
+            state1,
+            style: textStyle,
+          ),
+        ],
       ),
     );
   }
